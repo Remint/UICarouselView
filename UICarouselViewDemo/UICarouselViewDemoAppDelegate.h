7e071520bdf7cfb2ecc4a90652d1856407b2952b
Igor Starzhinskiy <Igor.Starzhinskiy@alterplay.com>
2011-11-01 09:53:38 +0300

    Initial Commit

---
+++ UICarouselViewDemo/UICarouselViewDemoAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  UICarouselViewDemoAppDelegate.h
+//  UICarouselViewDemo
+//
+//  Created by Igor Starzhinskiy on 11/1/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class UICarouselViewDemoViewController;
+
+@interface UICarouselViewDemoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UICarouselViewDemoViewController *viewController;
+
+@end
